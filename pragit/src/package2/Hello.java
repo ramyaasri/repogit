2a45e7f856e5c0c60cf4504dd4e615b94be5e6d1
ramyaasri <raamyaa.sri@gmail.com>
2021-05-25 10:57:33 +0530

    New Branch other Updated

--- pragit/src/package2/Hello.java
+++ pragit/src/package2/Hello.java
@@ -3,7 +3,8 @@ package package2;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hiii");
-		System.out.println("This is master");
+		System.out.println("This is other");
+		System.out.println("have a good day");
 
 	}
 
