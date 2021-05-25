ad4224e7764164c376d86b483103501c847c2732
ramyaasri <raamyaa.sri@gmail.com>
2021-05-25 11:01:45 +0530

    update

--- pragit/src/package2/Hello.java
+++ pragit/src/package2/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hiii");
 		System.out.println("This is other");
-		System.out.println("have a good day");
+		
 
 	}
 
