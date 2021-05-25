a411a01a7bfe05cd3c1520f690a8ee2e2bdd8822
ramyaasri <raamyaa.sri@gmail.com>
2021-05-25 10:35:38 +0530

    new Branch added

--- pragit/src/package2/Hello.java
+++ pragit/src/package2/Hello.java
@@ -3,6 +3,8 @@ package package2;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hiii");
+		System.out.println("This is other branch named change");
+		System.out.println("welcome to this branch ");
 	}
 
 }
