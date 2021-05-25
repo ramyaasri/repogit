d97e75f282bab286597beba9b541a76c61fc127e
ramyaasri <raamyaa.sri@gmail.com>
2021-05-25 11:00:29 +0530

    New Branch

--- pragit/src/package2/Hello.java
+++ pragit/src/package2/Hello.java
@@ -3,7 +3,7 @@ package package2;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hiii");
-		System.out.println("This is other");
+		System.out.println("This is new braanch");
 		System.out.println("have a good day");
 
 	}
